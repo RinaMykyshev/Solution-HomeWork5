3284330337730181b879019cbe23c698ad910ed4
Ваше Имя <_email@example.com>
2025-04-05 21:01:27 +0500

    created general MAIN file

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
 package src;
+import src.proxy.Image;
+import src.proxy.ProxyImage;
+import src.proxy.ProtectedImageUploader;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("--- Viewing Image ---");
+        Image img = new ProxyImage("house1.jpg");
+        img.displayThumbnail();
+        img.displayFullImage();
+
+        System.out.println("\n--- Upload Access ---");
+        ProtectedImageUploader uploader = new ProtectedImageUploader(false);
+        uploader.uploadImage("newHouse.jpg");
+
+        uploader = new ProtectedImageUploader(true);
+        uploader.uploadImage("newHouse.jpg");
+    }
 }
