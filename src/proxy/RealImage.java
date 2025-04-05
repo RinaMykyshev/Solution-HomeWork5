8a539b70525ec106787bc64d10ff9d55e6cb4cc6
Ваше Имя <_email@example.com>
2025-04-05 20:36:17 +0500

    created RealImage

---
+++ src/proxy/RealImage.java
@@ -0,0 +1,20 @@
+package src.proxy;
+
+public class RealImage implements Image {
+    private String filename;
+    public RealImage(String filename) {
+        this.filename = filename;
+        loadFromDisk();
+    }
+    private void loadFromDisk() {
+        System.out.println("Loading full image from disk: " + filename);
+    }
+    @Override
+    public void displayThumbnail() {
+        System.out.println("Displaying thumbnail of: " + filename);
+    }
+    @Override
+    public void displayFullImage() {
+        System.out.println("Displaying FULL image: " + filename);
+    }
+}
